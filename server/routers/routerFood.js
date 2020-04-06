861847909037df35b623c344434e4b31cce65b37
mohhaidir <khoiruljmbr@gmail.com>
2020-04-06 12:02:18 +0700

    Mohammad Haidir done

--- server/routers/routerFood.js
+++ server/routers/routerFood.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const ControllerFoods = require('../controllers/controllerFood')
+const Authentication = require('../middlewares/authentication')
+const Authorization = require('../middlewares/authorization')
+const errorHandler = require('../middlewares/errorHandler')
 
-router.get('/', ControllerFoods)
+// router.get('/', ControllerFoods.GetFoods)
+// router.post('/', ControllerFoods.AddFoods)
 
 module.exports = router
--- server/routers/routerUser.js
+++ server/routers/routerUser.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const ControllerUsers = require('../controllers/controllerUser')
-// const Authentication = require('../middlewares/authentication')
-// const Authorization = require('../middlewares/authorization')
-// const errorHandler = require('../middlewares/errorHandler')
+const errorHandler = require('../middlewares/errorHandler')
 
 router.post('/register', ControllerUsers.register)
 router.post('/login', ControllerUsers.login)
 
+router.use(errorHandler)
 module.exports = router
